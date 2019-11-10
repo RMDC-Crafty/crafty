bd56b128a317f3fe7cd1405515d3ed4c8815a981
Ptarrant <ptarrant@gmail.com>
2019-11-09 23:14:46 -0500

    adding quotes to launch string because I hate windows and spaces in path names

--- app/classes/minecraft_server.py
+++ app/classes/minecraft_server.py
@@ -103,7 +103,7 @@ class Minecraft_Server():
             self.server_command = self.server_command.replace('\\', '/')
             self.process = pexpect.popen_spawn.PopenSpawn('cmd \n', timeout=None, encoding=None)
             self.process.send('cd {} \n'.format(self.server_path.replace('\\', '/')))
-            self.process.send(self.server_command + "\n")
+            self.process.send('"' + self.server_command + '"' + "\n")
             self.PID = self.process.pid
 
         else:
