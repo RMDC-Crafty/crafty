b3ca8928cf8e3b36ec3da16c95283274052200d5
Ptarrant <ptarrant@gmail.com>
2019-12-31 14:07:26 -0500

    adding path / jar validation to setup handler

--- app/classes/handlers/setup_handler.py
+++ app/classes/handlers/setup_handler.py
@@ -23,11 +23,14 @@ class SetupHandler(BaseHandler):
         context = {}
         template = ''
 
+        errors = self.get_argument('errors', None)
+
         if page == 'step1':
             context = {
                 'is_windows': helper.is_os_windows(),
                 'mem': helper.get_memory(),
-                'new_pass': helper.random_string_generator(10)
+                'new_pass': helper.random_string_generator(10),
+                'errors': errors
                 }
             template = "setup/step1.html"
             helper.del_file(helper.new_install_file)
@@ -51,42 +54,54 @@ class SetupHandler(BaseHandler):
             min_mem = self.get_argument('min_mem', '')
             auto_start = self.get_argument('auto_start', '')
 
-            MC_settings.insert({
-                MC_settings.server_name: server_name,
-                MC_settings.server_path: server_path,
-                MC_settings.server_jar: server_jar,
-                MC_settings.memory_max: max_mem,
-                MC_settings.memory_min: min_mem,
-                MC_settings.additional_args: "",
-                MC_settings.auto_start_server: auto_start,
-                MC_settings.auto_start_delay: 10,
-                MC_settings.auto_start_priority: 1,
-                MC_settings.crash_detection: 0,
-                MC_settings.server_port: 25565,
-                MC_settings.server_ip: "127.0.0.1"
-            }).execute()
+            server_path_exists = helper.check_directory_exist(server_path)
 
-            directories = [server_path, ]
-            backup_directory = json.dumps(directories)
+            if not server_path_exists:
+                self.redirect('/setup/step1.html?errors=Server Path not found')
 
-            # default backup settings
-            Backups.insert({
-                Backups.directories: backup_directory,
-                Backups.storage_location: os.path.abspath(os.path.join(helper.crafty_root, 'backups')),
-                Backups.max_backups: 7
-            }).execute()
+            # Use pathlib to join specified server path and server JAR file then check if it exists
+            jar_exists = helper.check_file_exists(os.path.join(server_path, server_jar))
 
-            time.sleep(.5)
+            if not jar_exists:
+                self.redirect('/setup/step1.html?errors=Server Jar not found')
 
-            # do initial setup
-            multi.init_all_servers()
+            if server_path_exists and jar_exists:
+                MC_settings.insert({
+                    MC_settings.server_name: server_name,
+                    MC_settings.server_path: server_path,
+                    MC_settings.server_jar: server_jar,
+                    MC_settings.memory_max: max_mem,
+                    MC_settings.memory_min: min_mem,
+                    MC_settings.additional_args: "",
+                    MC_settings.auto_start_server: auto_start,
+                    MC_settings.auto_start_delay: 10,
+                    MC_settings.auto_start_priority: 1,
+                    MC_settings.crash_detection: 0,
+                    MC_settings.server_port: 25565,
+                    MC_settings.server_ip: "127.0.0.1"
+                }).execute()
 
-            # reload the server settings
-            srv_obj = multi.get_first_server_object()
-            srv_obj.reload_settings()
+                directories = [server_path, ]
+                backup_directory = json.dumps(directories)
 
-            # do FTP setup
-            ftp_svr_object.setup_ftp()
+                # default backup settings
+                Backups.insert({
+                    Backups.directories: backup_directory,
+                    Backups.storage_location: os.path.abspath(os.path.join(helper.crafty_root, 'backups')),
+                    Backups.max_backups: 7
+                }).execute()
 
-            # load the dashboard
-            self.redirect("/admin/dashboard")
+                time.sleep(.5)
+
+                # do initial setup
+                multi.init_all_servers()
+
+                # reload the server settings
+                srv_obj = multi.get_first_server_object()
+                srv_obj.reload_settings()
+
+                # do FTP setup
+                ftp_svr_object.setup_ftp()
+
+                # load the dashboard
+                self.redirect("/admin/dashboard")
