a1f6ff916c0a9bf60e920b20e580049c049875d3
Ptarrant <ptarrant@gmail.com>
2019-12-31 13:49:18 -0500

    moved the handlers to their own folder to help breakup http.py added better server path / jar validation

---
+++ app/classes/handlers/public_handler.py
@@ -0,0 +1,87 @@
+import logging
+import tornado.web
+import tornado.escape
+
+from app.classes.console import console
+from app.classes.models import *
+from app.classes.handlers.base_handler import BaseHandler
+
+
+class PublicHandler(BaseHandler):
+
+    def initialize(self):
+        self.console = console
+
+    def set_current_user(self, user):
+        if user:
+            self.set_secure_cookie("user", tornado.escape.json_encode(user), expires_days=1)
+        else:
+            self.clear_cookie("user")
+
+    def get(self, page=None):
+
+        self.clear_cookie("user")
+
+        server_data = self.get_server_data()
+
+        template = "public/login.html"
+        context = server_data
+        context['login'] = None
+
+        self.render(
+            template,
+            data=context
+        )
+
+
+    def post(self):
+        entered_user = self.get_argument('username')
+        entered_password = self.get_argument('password')
+
+
+        try:
+            user_data = Users.get(Users.username == entered_user)
+
+            if user_data:
+                # if the login is good and the pass verified, we go to the dashboard
+                login_result = helper.verify_pass(entered_password, user_data.password)
+                if login_result:
+                    self.set_current_user(entered_user)
+
+                    if helper.check_file_exists(helper.new_install_file):
+                        next_page = "/setup/step1"
+                    else:
+                        next_page = '/admin/dashboard'
+
+                    self.redirect(next_page)
+        except:
+            pass
+
+        server_data = self.get_server_data()
+
+        template = "public/login.html"
+        context = server_data
+        context['login'] = False
+
+        self.render(
+            template,
+            data=context
+        )
+
+
+
+    def get_server_data(self):
+        server_file = os.path.join( helper.get_web_temp_path(), "server_data.json")
+
+        if helper.check_file_exists(server_file):
+            with open(server_file, 'r') as f:
+                server_data = json.load(f)
+            return server_data
+        else:
+            logger.warning("Unable to find server_data file for dashboard: {}".format(server_file))
+            fake_data = {
+                "server_description": "Unable To Connect",
+                "server_running": False,
+                "server_version": "Unable to Connect",
+            }
+            return fake_data
---
+++ app/classes/handlers/setup_handler.py
@@ -0,0 +1,92 @@
+import time
+import tornado.web
+import tornado.escape
+from pathlib import Path
+
+from app.classes.console import console
+from app.classes.models import *
+from app.classes.handlers.base_handler import BaseHandler
+from app.classes.multiserv import multi
+from app.classes.ftp import ftp_svr_object
+
+
+
+class SetupHandler(BaseHandler):
+
+    def initialize(self, mcserver):
+        self.mcserver = mcserver
+        self.console = console
+
+    @tornado.web.authenticated
+    def get(self, page):
+
+        context = {}
+        template = ''
+
+        if page == 'step1':
+            context = {
+                'is_windows': helper.is_os_windows(),
+                'mem': helper.get_memory(),
+                'new_pass': helper.random_string_generator(10)
+                }
+            template = "setup/step1.html"
+            helper.del_file(helper.new_install_file)
+
+        else:
+            # 404
+            template = "public/404.html"
+            context = {}
+
+        self.render(
+            template,
+            data=context
+        )
+
+    def post(self, page):
+        if page == 'step1':
+            server_name = self.get_argument('server_name', '')
+            server_path = self.get_argument('server_path', '')
+            server_jar = self.get_argument('server_jar', '')
+            max_mem = self.get_argument('max_mem', '')
+            min_mem = self.get_argument('min_mem', '')
+            auto_start = self.get_argument('auto_start', '')
+
+            MC_settings.insert({
+                MC_settings.server_name: server_name,
+                MC_settings.server_path: server_path,
+                MC_settings.server_jar: server_jar,
+                MC_settings.memory_max: max_mem,
+                MC_settings.memory_min: min_mem,
+                MC_settings.additional_args: "",
+                MC_settings.auto_start_server: auto_start,
+                MC_settings.auto_start_delay: 10,
+                MC_settings.auto_start_priority: 1,
+                MC_settings.crash_detection: 0,
+                MC_settings.server_port: 25565,
+                MC_settings.server_ip: "127.0.0.1"
+            }).execute()
+
+            directories = [server_path, ]
+            backup_directory = json.dumps(directories)
+
+            # default backup settings
+            Backups.insert({
+                Backups.directories: backup_directory,
+                Backups.storage_location: os.path.abspath(os.path.join(helper.crafty_root, 'backups')),
+                Backups.max_backups: 7
+            }).execute()
+
+            time.sleep(.5)
+
+            # do initial setup
+            multi.init_all_servers()
+
+            # reload the server settings
+            srv_obj = multi.get_first_server_object()
+            srv_obj.reload_settings()
+
+            # do FTP setup
+            ftp_svr_object.setup_ftp()
+
+            # load the dashboard
+            self.redirect("/admin/dashboard")
