b97169514e2cb9d86c8e505bf3f9435d99b99bf3
computergeek125 <computergeek125@gmail.com>
2021-02-27 17:19:11 -0600

    add Permissions-Policy and CSP

--- app/classes/handlers/base_handler.py
+++ app/classes/handlers/base_handler.py
@@ -5,8 +5,8 @@ logger = logging.getLogger(__name__)
 
 class BaseHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
-        #self.set_header("Permissions-Policy
-        #self.set_header("Content-Security-Policy
+        self.set_header("Permissions-Policy", "")
+        self.set_header("Content-Security-Policy", "default-src 'self' https:; script-src 'self' https: 'unsafe-inline'; style-src 'self' https: 'unsafe-inline'")
         self.set_header("Strict-Transport-Security", "max-age=31536000")
         self.set_header("X-XSS-Protection", "1; mode=block")
         self.set_header("X-Frame-Options", "DENY")
